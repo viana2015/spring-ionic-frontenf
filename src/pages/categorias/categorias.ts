552ba6c2764e524656447ae9c745f104a401b1f4
Cristiano Viana <cristiano.gvianati@gmail.com>
2020-06-09 18:55:52 -0300

    Mostrando as Categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoriaService } from '../../services/domain/categoria.service';
+import { CategoriaDTO } from '../../models/categoria.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 /**
  * Generated class for the CategoriasPage page.
@@ -16,6 +18,9 @@ import { CategoriaService } from '../../services/domain/categoria.service';
 })
 export class CategoriasPage {
 
+  bucketUrl: String = API_CONFIG.bucketBaseUrl;
+  items: CategoriaDTO[]; //Lendo informações de Categorias do backend passando para o html da nossa pagina ou app
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
@@ -25,7 +30,8 @@ export class CategoriasPage {
   ionViewDidLoad() {
     this.categoriaService.buscarTodas()
       .subscribe(response => {
-        console.log(response);
+        //console.log(response);
+        this.items = response;
       },
       error => {
         console.log(error)
