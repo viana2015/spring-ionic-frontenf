5b3e19ad18a057bc8416b2442951d6b04e39d86f
Cristiano Viana <cristiano.gvianati@gmail.com>
2020-06-10 21:18:54 -0300

    Mostrando dados e imagem do cliente para pagina de profile

---
+++ src/models/cliente.dto.ts
@@ -0,0 +1,7 @@
+export interface ClienteDTO {
+
+    id : String;
+    nome : String;
+    email : String;
+    imageUrl? : String;
+}
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { StorageService } from '../../services/storage.service';
+import { ClienteDTO } from '../../models/cliente.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -9,17 +12,30 @@ import { StorageService } from '../../services/storage.service';
 })
 export class ProfilePage {
 
-  email: String;
+  cliente: ClienteDTO;
 
   // Injeção de dependencias 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: StorageService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: StorageService,
+    public clienteService: ClienteService) {
   }
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser(); //se existir este localUser a variavél estara valendo
     if (localUser && localUser.email) { // testo se o localUser possui um email 
-      this.email = localUser.email; //email exitindo eu chamo o proprio email
+      this.clienteService.findByEmail(localUser.email)
+      .subscribe(response => {
+        this.cliente = response;
+        this.getImageIfExists();
+      },
+      error => {});
     }
   }
 
+  getImageIfExists(){
+    this.clienteService.getImageFromBucket(this.cliente.id)
+    .subscribe(response => {
+      this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`
+    },
+    error => {});
+  }
 }
